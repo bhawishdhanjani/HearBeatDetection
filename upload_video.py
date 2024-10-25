259507818ee5bc18c4bbda29cae70e08c7647549
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2024-10-25 19:28:45 +0500

    first commit

---
+++ upload_video.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, File, UploadFile
+import shutil
+from pathlib import Path
+from PyQt5.QtWidgets import QApplication
+from mainwindow import MainWindow
+from rppg import RPPG
+import uuid
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this list to specify which origins are allowed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Define the path where you want to store uploaded videos
+UPLOAD_DIR = Path("./uploaded_videos")
+UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
+@app.post("/upload-video/")
+async def upload_video(file: UploadFile = File(...)):
+    # Define file path for saving
+    file_name = str(uuid.uuid4())+"."+str.split(file.filename,".")[1]
+    file_path = UPLOAD_DIR / file_name
+    
+    # Save the file
+    with file_path.open("wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    app = QApplication([])
+
+    rppg = RPPG(parent=app)
+
+    # Specify the path to your recorded video file
+    
+    average_heartbeat = rppg.process_video(file_path)
+
+    # print(f"Average Heartbeat: {average_heartbeat}")
+    
+    return {"filename": file.filename, "status": "Uploaded successfully","average_heartbeat" : average_heartbeat}
+
